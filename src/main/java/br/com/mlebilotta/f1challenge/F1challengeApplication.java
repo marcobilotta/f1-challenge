5780aa6a566d96ed7391a60e80649cf49c6ca726
Marco Bilotta <marco.bilotta@outlook.com>
2023-10-18 10:25:24 -0300

    feat: start the project f1-challenge

---
+++ src/main/java/br/com/mlebilotta/f1challenge/F1challengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.mlebilotta.f1challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1challengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1challengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mlebilotta/f1challenge/F1challengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mlebilotta.f1challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1challengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
