80d9fb7657087fb258b95392cf0bddfcbae5fa8d
Marco Bilotta <marco.bilotta@outlook.com>
2023-10-19 19:55:20 -0300

    fix: adjust cityOfBirth get method

--- src/main/java/br/com/mlebilotta/f1challenge/application/domain/entity/Driver.java
+++ src/main/java/br/com/mlebilotta/f1challenge/application/domain/entity/Driver.java
@@ -1,8 +1,8 @@
 package br.com.mlebilotta.f1challenge.application.domain.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
 
 @Table(name = "DRIVER")
 @Entity
@@ -11,7 +11,59 @@ public class Driver {
     @Id
     private String id;
     private String name;
+    private Double height;
     private String cityOfBirth;
     private String countryOfBirth;
     private String function;
+    private Boolean active;
+    private LocalDate createdAt;
+    private LocalDate lastModifiedAt;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public String getCityOfBirth() {
+        return cityOfBirth;
+    }
+
+    public String getCountryOfBirth() {
+        return countryOfBirth;
+    }
+
+    public String getFunction() {
+        return function;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+
+    public void setCityOfBirth(String cityOfBirth) {
+        this.cityOfBirth = cityOfBirth;
+    }
+
+    public void setCountryOfBirth(String countryOfBirth) {
+        this.countryOfBirth = countryOfBirth;
+    }
+
+    public void setFunction(String function) {
+        this.function = function;
+    }
 }
---
+++ src/main/java/br/com/mlebilotta/f1challenge/application/domain/service/DriverService.java
@@ -0,0 +1,4 @@
+package br.com.mlebilotta.f1challenge.application.domain.service;
+
+public class DriverService {
+}
