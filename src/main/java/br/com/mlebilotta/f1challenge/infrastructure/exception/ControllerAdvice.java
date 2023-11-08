80712c90461ad845b6e36a6dad70e253d54a30f2
Marco Bilotta <marco.bilotta@outlook.com>
2023-11-08 10:55:27 -0300

    feat: "Implementation of validation of mandatory fields via the Bean Validation library and return in the body with the captured fields and statusCode 400."  [feature/MLEBF1C002-CriarEndpointParaCadastrarPilotos]

--- src/main/java/br/com/mlebilotta/f1challenge/infrastructure/exception/ControllerAdvice.java
+++ src/main/java/br/com/mlebilotta/f1challenge/infrastructure/exception/ControllerAdvice.java
@@ -1,15 +1,28 @@
 package br.com.mlebilotta.f1challenge.infrastructure.exception;
 
+import br.com.mlebilotta.f1challenge.infrastructure.exception.response.ErrorFieldsResponse;
 import br.com.mlebilotta.f1challenge.infrastructure.exception.response.ErrorResponse;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.List;
+
+@Log4j2
 @RestControllerAdvice
 public class ControllerAdvice {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ErrorFieldsResponse>> methodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        var errors = ex.getFieldErrors();
+        log.error("DRIVER CONTROLLER > driverRegister > Driver não possui todos os campos obrigatórios - CAUSA: [{}]", errors.stream().map(ErrorFieldsResponse::new).toList());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors.stream().map(ErrorFieldsResponse::new).toList());
+    }
     @ExceptionHandler(CommonDriverException.class)
-    public ResponseEntity<ErrorResponse> CommonDriverException(CommonDriverException ex) {
+    public ResponseEntity<ErrorResponse> commonDriverException(CommonDriverException ex) {
         ErrorResponse error = ErrorResponse.builder()
                 .status(ex.getStatusCode().toString())
                 .message(ex.getMessage())
